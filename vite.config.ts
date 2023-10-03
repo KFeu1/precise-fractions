449639baea9b683d1cdda96293071c93242db2a7
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2023-10-03 18:34:26 +0200

    Add types to dist

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import { resolve } from 'path';
+import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [solid(), dts({
+    insertTypesEntry: true,
+  }),],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
