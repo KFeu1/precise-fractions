d7c0dad6d8f5f2c581b892dc9c387b774834f429
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2023-10-03 21:38:04 +0200

    Remove doc comments from build

--- vite.config.ts
+++ vite.config.ts
@@ -4,9 +4,13 @@ import { resolve } from 'path';
 import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [solid(), dts({
-    insertTypesEntry: true,
-  }),],
+  plugins: [
+    solid(),
+    dts({
+      insertTypesEntry: true,
+    }),
+    removeDocs(),
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
@@ -15,3 +19,44 @@ export default defineConfig({
     copyPublicDir: false,
   },
 })
+
+function removeDocs() {
+  return {
+    name: 'remove-docs',
+
+    transform(src, id) {
+      if (/\.ts/.test(id)) {
+        const rm = (js) => {
+          const lines = js.split(/\r?\n/);
+
+          let isDoc = false;
+          const out = [];
+
+          for (const line of lines) {
+            // console.log(line);
+            if (/^\s*\/\*\*/.test(line)) {
+              isDoc = true;
+            } else if (isDoc === true && /^\s*\*\//.test(line)) {
+              isDoc = false;
+              continue;
+            }
+
+            if (!isDoc) {
+              out.push(line);
+            }
+          }
+
+          const res = out.join("\n");
+          console.log("res", res)
+          return res;
+        }
+
+
+        return {
+          code: rm(src),
+          map: null, // provide source map if available
+        }
+      }
+    },
+  }
+}
