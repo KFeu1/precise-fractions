0bf06e1c38f531617bd5f954e5f933fca8892853
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2023-10-03 18:55:40 +0200

    Add some doc comments

--- lib/src/operations.ts
+++ lib/src/operations.ts
@@ -1,4 +1,9 @@
 
+/**
+ * Represents a fraction with numerator and denominator
+ *
+ * Cast it to a number to get the fractional value as a Javascript number type
+ */
 class Fraction {
     numerator: bigint;
     denominator: bigint;
@@ -23,11 +28,20 @@ class Fraction {
     }
 }
 
+/**
+ * Create a new fraction.
+ *
+ * If the denominator is omitted, it becomes 1 by default.
+ *
+ * @param numerator bigint | number | string
+ * @param denominator bigint | number | string
+ * @returns Fraction
+ */
 function createFraction(numerator: bigint | number | string, denominator?: bigint | number | string) {
     return new Fraction(numerator, denominator);
 }
 
-function gcd(a: bigint, b: bigint) : bigint {
+function gcd(a: bigint, b: bigint) {
     if (b === 0n) {
       return a;
     }
@@ -35,10 +49,17 @@ function gcd(a: bigint, b: bigint) : bigint {
     return gcd(b, a % b);
 }
 
-function lcm(a: bigint, b: bigint) : bigint {
+function lcm(a: bigint, b: bigint) {
     return a * b / gcd(a,b);
 }
 
+/**
+ * Add two fractions
+ *
+ * @param f1 Fraction
+ * @param f2 Fraction
+ * @returns Fraction
+ */
 function add(f1: Fraction, f2: Fraction) {
     const newDenominator = lcm(f1.denominator, f2.denominator);
     const factor1 = newDenominator / f1.denominator;
@@ -48,6 +69,13 @@ function add(f1: Fraction, f2: Fraction) {
     return new Fraction(newNumerator, newDenominator);
 }
 
+/**
+* Subtract a fraction from another
+*
+* @param f1 Fraction
+* @param f2 Fraction
+* @returns Fraction
+*/
 function subtract(f1: Fraction, f2: Fraction) {
     const newDenominator = lcm(f1.denominator, f2.denominator);
     const factor1 = newDenominator / f1.denominator;
@@ -57,14 +85,35 @@ function subtract(f1: Fraction, f2: Fraction) {
     return new Fraction(newNumerator, newDenominator);
 }
 
+/**
+ * Multiply two fractions
+ *
+ * @param f1 Fraction
+ * @param f2 Fraction
+ * @returns Fraction
+ */
 function multiply(f1: Fraction, f2: Fraction) {
     return new Fraction(f1.numerator * f2.numerator, f1.denominator * f2.denominator);
 }
 
+/**
+ * Divide a fraction by another
+ *
+ * @param f1 Fraction
+ * @param f2 Fraction
+ * @returns Fraction
+ */
 function divide(f1: Fraction, f2: Fraction) {
     return new Fraction(f1.numerator * f2.denominator, f1.denominator * f2.numerator);
 }
 
+/**
+ * Take a fraction to a power
+ *
+ * @param f Fraction
+ * @param power Exponent
+ * @returns Fraction
+ */
 function power(f: Fraction, power: bigint | number | string) {
     if (typeof power !== "bigint") {
         power = BigInt(power);
