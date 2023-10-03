36d2c11ae001cef80d7a05d915a16b0733b5d37f
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2023-10-03 20:28:27 +0200

    Fix build

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,6 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, 'lib/main.ts'),
-      formats: ['es'],
       name: 'fractionJS',
     },
     copyPublicDir: false,
