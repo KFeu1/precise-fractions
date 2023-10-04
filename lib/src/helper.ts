9d2e89d64ed041aba852cc2e579107a672883c54
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2023-10-04 23:24:33 +0200

    Refactor ternary

--- lib/src/helper.ts
+++ lib/src/helper.ts
@@ -1,6 +1,5 @@
 import { Fraction } from "./fraction-class";
 
 export function _toFraction(f: Fraction | bigint | number | string) {
-    if (f instanceof Fraction) return f;
-    return new Fraction(f);
+    return (f instanceof Fraction) ? f : new Fraction(f);
 }
