33550e0a4f1dbdd50c65bd2c45e87069df87f8f2
Konstantin Feuerborn <k.feuerborn@yahoo.de>
2025-02-24 18:07:36 +0100

    Edit utils

--- lib/src/utils.ts
+++ lib/src/utils.ts
@@ -4,12 +4,9 @@
  * @param a 
  * @param b 
  */
-function gcd(a: number, b: number): number;
-function gcd(a: bigint, b: bigint): bigint;
-function gcd<T extends bigint | number>(a: T, b: T): T {
+function gcd(a: bigint, b: bigint): bigint {
     while (b !== 0n) {
       const temp = b;
-      // @ts-expect-error
       b = a % b;
       a = temp;
     }
@@ -22,10 +19,7 @@ function gcd<T extends bigint | number>(a: T, b: T): T {
  * @param a 
  * @param b 
  */
-function lcm(a: number, b: number): number;
-function lcm(a: bigint, b: bigint): bigint;
-function lcm<T extends bigint | number>(a: T, b: T): T {
-    // @ts-expect-error
+function lcm(a: bigint, b: bigint): bigint {
     return (a / gcd(a,b)) * b;
 }
 
